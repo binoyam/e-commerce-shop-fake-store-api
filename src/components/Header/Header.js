fb18d0b43c8832a0975e50a18a26a941f8ef864c
binoyam <binyam.techan01@gmail.com>
2023-10-14 03:47:42 +0300

    header/nav

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "./Header.css";
 import Logo from "../../Assets/logo.svg";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header className="header">
       <div className="logo-div">
-        <img src={Logo} alt="Logo" className="logo-image" />
-        <span className="logo-text">
-           e-Shop
-        </span>
+        <Link to="/" className="logo-link">
+          <img src={Logo} alt="Logo" className="logo-image" />
+          <span className="logo-text">e-Shop</span>
+        </Link>
       </div>
-      <nav>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+      <nav className="navigation">
+        <ul className="nav-links">
+          <li>
+            <Link to="/products" className="nav-link">Products</Link>
+          </li>
+          <li>
+            <Link to="/cart" className="nav-link">Cart</Link>
+          </li>
+          <li>
+            <Link to="/account" className="nav-link">Account</Link>
+          </li>
         </ul>
       </nav>
     </header>
