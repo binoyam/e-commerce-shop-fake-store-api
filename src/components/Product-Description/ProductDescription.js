53ec476a8a78fd0f161646852ea166ac287ee542
binoyam <binyam.techan01@gmail.com>
2023-10-19 02:41:21 +0300

    product description

--- src/components/Product-Description/ProductDescription.js
+++ src/components/Product-Description/ProductDescription.js
@@ -1,8 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function ProductDescription({ product }) {
-    console.log(product)
-  return <div>ProductDescription</div>;
+  console.log(product);
+  const { id, title, price, description, category, rating } = product;
+  return (
+    <div className="product-description">
+      <Link to="/" className="">
+        <button className="back-btn">Back</button>
+      </Link>
+      <div className="product-info">
+        <h1>{title}</h1>
+        <p>{category}</p>
+        <p>{description}</p>
+        <p>{price}</p>
+        <p>{rating.count}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ProductDescription;
