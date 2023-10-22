7c42e4e2965ef46ba729f7ade4dc9b1055e4d088
binoyam <binyam.techan01@gmail.com>
2023-10-23 01:24:29 +0300

    order total component

--- src/components/Checkout-OrderSummary/OrderSummary.js
+++ src/components/Checkout-OrderSummary/OrderSummary.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./OrderSummary.css";
 
 function OrderSummary({ cartItems }) {
@@ -11,29 +12,34 @@ function OrderSummary({ cartItems }) {
   };
 
   return (
-    <div className="order-summary-wrapper">
-      <div className="order-summary-header">Order Summary</div>
-      <div className="order-items">
-        <p className="left">Items ({cartItems.length}):</p>
-        <p>${calculateTotal().toFixed(2)}</p>
+
+      <div className="order-summary-wrapper">
+        <div className="order-summary-header">Order Summary</div>
+        <div className="order-items">
+          <p className="left">Items ({cartItems.length}):</p>
+          <p>${calculateTotal().toFixed(2)}</p>
+        </div>
+        <div className="shipping">
+          <p className="left">Shipping & handling:</p>
+          <p>$0.00</p>
+        </div>
+        <div className="before-tax">
+          <p className="left">Total before tax: </p>
+          <p>${calculateTotal().toFixed(2)}</p>
+        </div>
+        <div className="estimated-tax">
+          <p className="left">Estimated tax to be collected: </p>
+          <p>${(calculateTotal() * TAX_RATE).toFixed(2)}</p>
+        </div>
+        <div className="order-total">
+          <p>Order total:</p>
+          <p>${(calculateTotal() * TAX_RATE + calculateTotal()).toFixed(2)}</p>
+        </div>
+      <Link className="payment" to="/payment">
+        Proceed to payment
+      </Link>
       </div>
-      <div className="shipping">
-        <p className="left">Shipping & handling:</p>
-        <p>$0.00</p>
-      </div>
-      <div className="before-tax">
-        <p className="left">Total before tax: </p>
-        <p>${calculateTotal().toFixed(2)}</p>
-      </div>
-      <div className="estimated-tax">
-        <p className="left">Estimated tax to be collected: </p>
-        <p>${(calculateTotal() * TAX_RATE).toFixed(2)}</p>
-      </div>
-      <div className="order-total">
-        <p>Order total:</p>
-        <p>${(calculateTotal() * TAX_RATE + calculateTotal()).toFixed(2)}</p>
-      </div>
-    </div>
+   
   );
 }
 
