adfc654086e643a2fb5b2799e52cd6227b39b06e
binoyam <binyam.techan01@gmail.com>
2023-10-23 02:52:25 +0300

    responsive menu

--- src/components/Header-Nav/Nav.js
+++ src/components/Header-Nav/Nav.js
@@ -14,50 +14,56 @@ function Nav({ setSelectedCategory }) {
   };
   return (
     <nav className="navigation">
-      <button onClick={toggleMenu} className="menu-btn">
-        <img src={MenuIcon} alt="menu" />
-      </button>
-      <ul className="nav-links">
-        <li>
-          <Link
-            onClick={() => setSelectedCategory("men%27s%20clothing")}
-            to="/category/mens-clothing"
-            className="nav-link"
-          >
-            Men
-          </Link>
-        </li>
-        <li>
-          <Link
-            onClick={() => setSelectedCategory("women%27s%20clothing")}
-            to="/category/womens-clothing"
-            className="nav-link"
-          >
-            Women
-          </Link>
-        </li>
-        <li>
-          <Link
-            onClick={() => setSelectedCategory("electronics")}
-            to="/category/electronics"
-            className="nav-link"
-          >
-            Electronics
-          </Link>
-        </li>
-        <li>
-          <Link
-            onClick={() => setSelectedCategory("jewelery")}
-            to="/category/jewelery"
-            className="nav-link"
-          >
-            Jewelery
-          </Link>
-        </li>
-      </ul>
-      <button onClick={closeMenu} className="close-menu-btn">
-        <img src={CloseMenuIcon} alt="close menu" />
-      </button>
+      {!isMenuOpen ? (
+        <button onClick={toggleMenu} className="menu-btn">
+          <img src={MenuIcon} alt="menu" />
+        </button>
+      ) : (
+        <button onClick={closeMenu} className="close-menu-btn">
+          <img src={CloseMenuIcon} alt="close menu" />
+        </button>
+      )}
+
+      {isMenuOpen && (
+        <ul className="nav-links">
+          <li>
+            <Link
+              onClick={() => setSelectedCategory("men%27s%20clothing")}
+              to="/category/mens-clothing"
+              className="nav-link"
+            >
+              Men
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setSelectedCategory("women%27s%20clothing")}
+              to="/category/womens-clothing"
+              className="nav-link"
+            >
+              Women
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setSelectedCategory("electronics")}
+              to="/category/electronics"
+              className="nav-link"
+            >
+              Electronics
+            </Link>
+          </li>
+          <li>
+            <Link
+              onClick={() => setSelectedCategory("jewelery")}
+              to="/category/jewelery"
+              className="nav-link"
+            >
+              Jewelery
+            </Link>
+          </li>
+        </ul>
+      )}
     </nav>
   );
 }
