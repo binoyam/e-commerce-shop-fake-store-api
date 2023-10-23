122a4eb2df96148384ec4b58ae96d6eaf7ea0210
binoyam <binyam.techan01@gmail.com>
2023-10-23 05:07:37 +0300

    user account

--- src/components/UserAccount/UserAccount.js
+++ src/components/UserAccount/UserAccount.js
@@ -3,13 +3,15 @@ import UserIcon from "../../Assets/avatar.svg";
 
 function UserAccount() {
   return (
-    <div className="user-account">
-
-      <div className="user-profile-pic">
-        <img src={UserIcon} alt="avatar" />
-      </div>
-     
+    <>
+    <div className="user-profile-pic">
+      <img src={UserIcon} alt="avatar" />
     </div>
+        
+    <div className="user-account-details">
+        <h1>You are not logged in</h1>
+    </div>
+    </>
   );
 }
 
