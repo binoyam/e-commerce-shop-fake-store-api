ba04f600452455440b515472c6482e99ef27cfb3
binoyam <binyam.techan01@gmail.com>
2023-10-23 05:24:09 +0300

    user account

--- src/components/UserAccount/UserAccount.js
+++ src/components/UserAccount/UserAccount.js
@@ -1,16 +1,25 @@
 import "./UserAccount.css";
 import UserIcon from "../../Assets/avatar.svg";
+import { useState } from "react";
 
 function UserAccount() {
+  const [isUserOpen, setIsUserOpen] = useState(false);
+  function toggleUser() {
+    setIsUserOpen(!isUserOpen);
+  }
   return (
     <>
-    <div className="user-profile-pic">
-      <img src={UserIcon} alt="avatar" />
-    </div>
-        
-    <div className="user-account-details">
+      <div onClick={toggleUser} className="user-profile-pic">
+        <img src={UserIcon} alt="avatar" />
+      </div>
+
+      <div
+        className={
+          isUserOpen ? "user-account-details" : "user-account-details show"
+        }
+      >
         <h1>You are not logged in</h1>
-    </div>
+      </div>
     </>
   );
 }
