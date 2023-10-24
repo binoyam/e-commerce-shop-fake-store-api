2fc11a41b6f33c74e5f755236263cca3f3622403
binoyam <binyam.techan01@gmail.com>
2023-10-24 04:16:02 +0300

    payment page

--- src/components/Payment/PaymentPage.js
+++ src/components/Payment/PaymentPage.js
@@ -1,6 +1,11 @@
+import { useState } from "react";
 import "./PaymentPage.css";
-import PpIcon from "../../Assets/paypal-icon.svg";
+// import PpIcon from "../../Assets/paypal-icon.svg";
 function PaymentPage({ cartItems }) {
+  const [cardNumber, setCardNumber] = useState("");
+  const [expiryDate, setExpiryDate] = useState("");
+  const [cvv, setCvv] = useState("");
+
   const TAX_RATE = 0.15;
   const calculateTotal = () => {
     let total = 0;
@@ -9,15 +14,48 @@ function PaymentPage({ cartItems }) {
     });
     return total;
   };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setCardNumber("");
+    setExpiryDate("");
+    setCvv("");
+  };
   return (
     <div className="payment-page">
-      <div className="div">
-        <p>Total</p>
-        <p>{calculateTotal()}</p>
+      <div className="total-payment">
+        <p className="ttl">Total</p>
+        <p>${(calculateTotal() * TAX_RATE + calculateTotal()).toFixed(2)}</p>
       </div>
-      <form action=""></form>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Card Number:
+          <input
+            type="text"
+            value={cardNumber}
+            onChange={(e) => setCardNumber(e.target.value)}
+          />
+        </label>
+        <label>
+          Expiry Date:
+          <input
+            type="text"
+            value={expiryDate}
+            onChange={(e) => setExpiryDate(e.target.value)}
+          />
+        </label>
+        <label>
+          CVV:
+          <input
+            type="text"
+            value={cvv}
+            onChange={(e) => setCvv(e.target.value)}
+          />
+        </label>
+        <button type="submit">Pay Now</button>
+      </form>
 
-      <img src={PpIcon} alt="paypal" />
+      {/* <img src={PpIcon} alt="paypal" /> */}
     </div>
   );
 }
