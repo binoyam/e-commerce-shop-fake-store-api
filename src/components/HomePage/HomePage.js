b61921635ac8804b899671858b21dde9ed97639a
binoyam <binyam.techan01@gmail.com>
2023-10-29 02:03:07 +0300

    home page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,6 +1,6 @@
-import { Link } from "react-router-dom";
 import "./HomePage.css";
 import { useState, useEffect } from "react";
+import TopProducts from "../TopProducts/TopProducts";
 
 // add a price filtering function in this page
 // write an about section
@@ -34,37 +34,28 @@ function HomePage({ products }) {
 
   return (
     <div className="home-page">
-      <div className="home-page-header">
+      <section className="home-page-header">
         <h1 className="home-header">Welcome to bt-shop!</h1>
         <p className="home-sub-header">
           Explore our wide range of products and start shopping today!
         </p>
-      </div>
-      <div className="slider-container">
+      </section>
+      <section className="slider-container">
         <h2 className="trending-txt">Top Rated Products</h2>
         <div className="slide-wrapper">
           {topProducts.map((product, index) => (
-            <div
+            <TopProducts
               key={product.id}
-              className={`slider-item ${
-                index === currentSlide ? "active" : ""
-              }`}
-            >
-              <h3 className="trending-title">{product.title.slice(0, 12)}</h3>
-              <div className="rating">
-                Customer rating: {product.rating.rate}
-              </div>
-
-              <img className="img" src={product.image} alt="prodcut" />
-              <span className="trending-price">${product.price}</span>
-              <Link className="link" to={`/product/${product.id}`}>
-                View <br />
-                Product
-              </Link>
-            </div>
+              index={index}
+              product={product}
+              currentSlide={currentSlide}
+            />
           ))}
         </div>
-      </div>
+      </section>
+      <section>
+        
+      </section>
     </div>
   );
 }
