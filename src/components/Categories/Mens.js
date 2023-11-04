821fb518a4272f8773806894150ecfcb06b2a907
binoyam <binyam.techan01@gmail.com>
2023-11-05 00:42:28 +0300

    clean up

--- src/components/Categories/Mens.js
+++ src/components/Categories/Mens.js
@@ -5,11 +5,7 @@ function Mens({ products, addToCart }) {
   const filteredProducts = products.filter(
     (product) => product.category === "men's clothing"
   );
-  return (
-    <>
-      <ProductList products={filteredProducts} addToCart={addToCart} />
-    </>
-  );
+  return <ProductList products={filteredProducts} addToCart={addToCart} />;
 }
 
 export default Mens;
