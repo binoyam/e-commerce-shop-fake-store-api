2d5d2434261c09b22b6862bb1627e7b842958f01
binoyam <binyam.techan01@gmail.com>
2023-11-05 00:12:17 +0300

    clean code for category header

--- src/components/CategoriesHeader/CategoriesHeader.js
+++ src/components/CategoriesHeader/CategoriesHeader.js
@@ -1,59 +1,41 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./CategoriesHeader.css";
 
 function CategoriesHeader() {
   const [categoryName, setCategoryName] = useState("All");
- 
+  const location = useLocation();
+  const paths = {
+    All: "/categories/all",
+    Men: "/categories/mens-clothing",
+    Women: "/categories/womens-clothing",
+    Electronics: "/categories/electronics",
+    Jewelery: "/categories/jewelery",
+  };
+  useEffect(() => {
+    const pagePath = location.pathname;
+    const category = Object.keys(paths).find((key) => paths[key] === pagePath);
+    setCategoryName(category || "All");
+  }, [location]);
+
+  console.log(categoryName);
   return (
     <div className="categories">
       <h2 className="categories-title">{categoryName}</h2>
       <ul className="category-links">
-        <li>
-          <Link
-            className="prd-category-link"
-            to="/categories/all"
-            onClick={() => setCategoryName("All")}
-          >
-            All
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="prd-category-link"
-            to="/categories/mens-clothing"
-            onClick={() => setCategoryName("Men")}
-          >
-            Men
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="prd-category-link"
-            to="/categories/womens-clothing"
-            onClick={() => setCategoryName("Women")}
-          >
-            Women
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="prd-category-link"
-            to="/categories/electronics"
-            onClick={() => setCategoryName("Electronics")}
-          >
-            Electronics
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="prd-category-link"
-            to="/categories/jewelery"
-            onClick={() => setCategoryName("Jewelery")}
-          >
-            Jewelery
-          </Link>
-        </li>
+        {Object.keys(paths).map((key) => (
+          <li key={key}>
+            <Link
+              className={`prd-category-link ${
+                categoryName === key ? "selected" : ""
+              }`}
+              to="/categories/all"
+              onClick={() => setCategoryName(key)}
+            >
+              {key}
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   );
