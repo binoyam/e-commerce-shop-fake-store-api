6c309eda18ba71fe520950ed1ad2dd4c82dc4fc7
binoyam <binyam.techan01@gmail.com>
2023-11-07 23:30:45 +0300

    home page images grid

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,29 +10,29 @@ function Hero() {
     <section className="grid-images-container">
       <div className="grid grid-one">
         <Link to="/categories/mens-clothing">
-          <div className="over-lay"></div>
-          <img src={MensImg} alt="Mens-clothing" />
+          <div className="over-lay one"></div>
+          <img className='men' src={MensImg} alt="Mens-clothing" />
           <p className="grid-title">Men's Clothing</p>
         </Link>
       </div>
       <div className="grid grid-two">
         <Link to="/categories/womens-clothing">
-          <div className="over-lay"></div>
-          <img src={WomensImg} alt="Womens-clothing" />
+          <div className="over-lay two"></div>
+          <img className='women' src={WomensImg} alt="Womens-clothing" />
           <p className="grid-title">Women's Clothing</p>
         </Link>
       </div>
       <div className="grid grid-three">
         <Link to="/categories/electronics">
-          <div className="over-lay"></div>
-          <img src={ElectronicsImg} alt="Electronics" />
+          <div className="over-lay three"></div>
+          <img className='electronics' src={ElectronicsImg} alt="Electronics" />
           <p className="grid-title">Electronics</p>
         </Link>
       </div>
       <div className="grid grid-four">
         <Link to="/categories/jewelery">
-          <div className="over-lay"></div>
-          <img src={JeweleryImg} alt="Jewelery" />
+          <div className="over-lay four"></div>
+          <img className='jewelery' src={JeweleryImg} alt="Jewelery" />
           <p className="grid-title">Jewelery</p>
         </Link>
       </div>
