ea6ccf9a82c3a3db066ab4be9de7261a4cad8995
binoyam <binyam.techan01@gmail.com>
2023-11-08 00:17:09 +0300

    home page images grid styled

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,38 +1,46 @@
-import './Hero.css';
-import { Link } from 'react-router-dom';
-import MensImg from '../../Assets/Images/mens-clothing.jpg'
-import WomensImg from '../../Assets/Images/womens-clothing-1.jpg'
-import ElectronicsImg from '../../Assets/Images/electronics.jpg'
-import JeweleryImg from '../../Assets/Images/jewelery.jpg'
+import "./Hero.css";
+import { Link } from "react-router-dom";
+import MensImg from "../../Assets/Images/mens-clothing-1.jpg";
+import WomensImg from "../../Assets/Images/womes-clothing.jpg";
+import ElectronicsImg from "../../Assets/Images/electronics-1.jpg";
+import JeweleryImg from "../../Assets/Images/jewelery-1.jpg";
 
 function Hero() {
   return (
     <section className="grid-images-container">
-      <div className="grid grid-one">
+      <div className="grid men-grid">
         <Link to="/categories/mens-clothing">
           <div className="over-lay one"></div>
-          <img className='men' src={MensImg} alt="Mens-clothing" />
+          <img className="men-grid-img" src={MensImg} alt="Mens-clothing" />
           <p className="grid-title">Men's Clothing</p>
         </Link>
       </div>
-      <div className="grid grid-two">
+      <div className="grid women-grid">
         <Link to="/categories/womens-clothing">
           <div className="over-lay two"></div>
-          <img className='women' src={WomensImg} alt="Womens-clothing" />
+          <img
+            className="women-grid-img"
+            src={WomensImg}
+            alt="Womens-clothing"
+          />
           <p className="grid-title">Women's Clothing</p>
         </Link>
       </div>
-      <div className="grid grid-three">
+      <div className="grid etrn-grid">
         <Link to="/categories/electronics">
           <div className="over-lay three"></div>
-          <img className='electronics' src={ElectronicsImg} alt="Electronics" />
+          <img
+            className="electronics-grid-img"
+            src={ElectronicsImg}
+            alt="Electronics"
+          />
           <p className="grid-title">Electronics</p>
         </Link>
       </div>
-      <div className="grid grid-four">
+      <div className="grid jwl-grid">
         <Link to="/categories/jewelery">
           <div className="over-lay four"></div>
-          <img className='jewelery' src={JeweleryImg} alt="Jewelery" />
+          <img className="jewelery-grid-img" src={JeweleryImg} alt="Jewelery" />
           <p className="grid-title">Jewelery</p>
         </Link>
       </div>
